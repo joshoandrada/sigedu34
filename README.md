6a48ab1178c55271056559b4909001fd136e1455
joshoandrada <joseluisandrada2002@gmail.com>
2019-08-25 23:00:44 -0300

    Version 0.0.1.

---
+++ README.md
@@ -0,0 +1,4 @@
+sigedu34
+========
+
+A Symfony project created on July 31, 2019, 3:48 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/index.html.twig');
+        // , [ 'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, ]
+    }
+    /**
+     * @Route("/nosotros", name="nosotros")
+     */
+    public function nosotrosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/nosotros.html.twig');
+        // , [ 'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, ]
+    }
+    /**
+     * @Route("/ubicacion", name="ubicacion")
+     */
+    public function ubicacionAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/ubicacion.html.twig');
+        // , [ 'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, ]
+    }
+
+
+}
